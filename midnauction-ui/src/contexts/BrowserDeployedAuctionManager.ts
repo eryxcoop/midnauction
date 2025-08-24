146bbba2cdf53c33d4e3a27750b95b01920fea37
jdiaz_eryx <jdiaz@eryx.co>
2025-08-24 02:51:52 -0300

    WIP: Fix connectToWallet

--- midnauction-ui/src/contexts/BrowserDeployedAuctionManager.ts
+++ midnauction-ui/src/contexts/BrowserDeployedAuctionManager.ts
@@ -71,6 +71,7 @@ export class BrowserDeployedAuctionManager implements DeployedAuctionAPIProvider
   }
 
   resolve(contractAddress?: ContractAddress): Observable<AuctionDeployment> {
+    debugger
     const deployment = new BehaviorSubject<AuctionDeployment>({ tag: 'undeployed' });
 
     this.#auctionDeployments.push(deployment);
@@ -105,6 +106,7 @@ export class BrowserDeployedAuctionManager implements DeployedAuctionAPIProvider
     try {
       deployment.next({ tag: 'deploying' });
       const providers = await this.getProviders();
+      debugger
       const api = await AuctionAPI.deploy(providers, this.logger);
       deployment.next({ tag: 'deployed', api });
     } catch (error) {
@@ -169,36 +171,115 @@ const initializeProviders = async (logger: Logger): Promise<AuctionProviders> =>
   };
 };
 
+// /** @internal */
+// const connectToWallet = (logger: Logger): Promise<{ wallet: DAppConnectorWalletAPI; uris: ServiceUriConfig }> => {
+//   debugger
+//   return fnPipe(
+//     interval(1000),
+//     map(() => window.midnight?.mnLace),
+//     // concatMap(() => {
+//     //   const dappConnectorAPI: DAppConnectorAPI | undefined = window.midnight?.dappConnectorAPI;
+//     //
+//     //   if (!dappConnectorAPI) {
+//     //     return throwError(() => new Error('DApp connector API not found'));
+//     //   }
+//     //
+//     //   return of(dappConnectorAPI);
+//     // }),
+//     concatMap((dappConnectorAPI) => dappConnectorAPI.walletAPI()),
+//     concatMap((walletAPI) => {
+//       if (!walletAPI) {
+//         return throwError(() => new Error('Wallet API not available'));
+//       }
+//
+//       return walletAPI.serviceUriConfig().then((uris) => ({ wallet: walletAPI, uris }));
+//     }),
+//     timeout(30000),
+//     take(1),
+//     tap(({ wallet, uris }) => {
+//       logger.info({ uris }, 'Connected to wallet');
+//     }),
+//     catchError((error) => {
+//       logger.error({ error }, 'Failed to connect to wallet');
+//       return throwError(() => error);
+//     }),
+//     firstValueFrom,
+//   );
+// };
 /** @internal */
 const connectToWallet = (logger: Logger): Promise<{ wallet: DAppConnectorWalletAPI; uris: ServiceUriConfig }> => {
-  return fnPipe(
-    interval(1000),
-    concatMap(() => {
-      const dappConnectorAPI: DAppConnectorAPI | undefined = (window as any).midnight?.dappConnectorAPI;
+  const COMPATIBLE_CONNECTOR_API_VERSION = '1.x';
 
-      if (!dappConnectorAPI) {
-        return throwError(() => new Error('DApp connector API not found'));
-      }
+  return firstValueFrom(
+      fnPipe(
+          interval(100),
+          map(() => window.midnight?.mnLace),
+          tap((connectorAPI) => {
+            logger.info(connectorAPI, 'Check for wallet connector API');
+          }),
+          filter((connectorAPI): connectorAPI is DAppConnectorAPI => !!connectorAPI),
+          concatMap((connectorAPI) =>
+              semver.satisfies(connectorAPI.apiVersion, COMPATIBLE_CONNECTOR_API_VERSION)
+                  ? of(connectorAPI)
+                  : throwError(() => {
+                    logger.error(
+                        {
+                          expected: COMPATIBLE_CONNECTOR_API_VERSION,
+                          actual: connectorAPI.apiVersion,
+                        },
+                        'Incompatible version of wallet connector API',
+                    );
 
-      return of(dappConnectorAPI);
-    }),
-    concatMap((dappConnectorAPI) => dappConnectorAPI.walletAPI()),
-    concatMap((walletAPI) => {
-      if (!walletAPI) {
-        return throwError(() => new Error('Wallet API not available'));
-      }
+                    return new Error(
+                        `Incompatible version of Midnight Lace wallet found. Require '${COMPATIBLE_CONNECTOR_API_VERSION}', got '${connectorAPI.apiVersion}'.`,
+                    );
+                  }),
+          ),
+          tap((connectorAPI) => {
+            logger.info(connectorAPI, 'Compatible wallet connector API found. Connecting.');
+          }),
+          take(1),
+          timeout({
+            first: 1_000,
+            with: () =>
+                throwError(() => {
+                  logger.error('Could not find wallet connector API');
 
-      return walletAPI.serviceUriConfig().then((uris) => ({ wallet: walletAPI, uris }));
-    }),
-    timeout(30000),
-    take(1),
-    tap(({ wallet, uris }) => {
-      logger.info({ uris }, 'Connected to wallet');
-    }),
-    catchError((error) => {
-      logger.error({ error }, 'Failed to connect to wallet');
-      return throwError(() => error);
-    }),
-    firstValueFrom,
+                  return new Error('Could not find Midnight Lace wallet. Extension installed?');
+                }),
+          }),
+          concatMap(async (connectorAPI) => {
+            const isEnabled = await connectorAPI.isEnabled();
+
+            logger.info(isEnabled, 'Wallet connector API enabled status');
+
+            return connectorAPI;
+          }),
+          timeout({
+            first: 5_000,
+            with: () =>
+                throwError(() => {
+                  logger.error('Wallet connector API has failed to respond');
+
+                  return new Error('Midnight Lace wallet has failed to respond. Extension enabled?');
+                }),
+          }),
+          concatMap(async (connectorAPI) => ({ walletConnectorAPI: await connectorAPI.enable(), connectorAPI })),
+          catchError((error, apis) =>
+              error
+                  ? throwError(() => {
+                    logger.error('Unable to enable connector API');
+                    return new Error('Application is not authorized');
+                  })
+                  : apis,
+          ),
+          concatMap(async ({ walletConnectorAPI, connectorAPI }) => {
+            const uris = await connectorAPI.serviceUriConfig();
+
+            logger.info('Connected to wallet connector API and retrieved service configuration');
+
+            return { wallet: walletConnectorAPI, uris };
+          }),
+      ),
   );
 };
